44e7940656dfd05fcd730c02a3f45acb9bc315f8
Dennis Weber <nijin22@gmail.com>
2017-03-12 17:50:28 +0100

    Archetype switch
    Switching Archetype to ninja-servlet-jpa-blog-archetype, because this
    one already has the in-memory DB set up and also includes a few more
    example files.

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -16,32 +16,57 @@
 
 package controllers;
 
+import java.util.List;
+import java.util.Map;
+
+import models.Article;
 import ninja.Result;
 import ninja.Results;
 
+import com.google.common.collect.Maps;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import dao.ArticleDao;
+import dao.SetupDao;
 
-@Singleton
 public class ApplicationController {
 
+    @Inject
+    ArticleDao articleDao;
+
+    @Inject
+    SetupDao setupDao;
+
+    public ApplicationController() {
+
+    }
+
+    /**
+     * Method to put initial data in the db...
+     * 
+     * @return
+     */
+    public Result setup() {
+
+        setupDao.setup();
+
+        return Results.ok();
+
+    }
+
     public Result index() {
-        return Results.html();
 
-    }
-    
-    public Result helloWorldJson() {
-        
-        SimplePojo simplePojo = new SimplePojo();
-        simplePojo.content = "Simple JSON test!";
+        Article frontPost = articleDao.getFirstArticleForFrontPage();
 
-        return Results.json().render(simplePojo);
+        List<Article> olderPosts = articleDao.getOlderArticlesForFrontPage();
 
-    }
-    
-    public static class SimplePojo {
+        Map<String, Object> map = Maps.newHashMap();
+        map.put("frontArticle", frontPost);
+        map.put("olderArticles", olderPosts);
 
-        public String content;
-        
+        return Results.html().render("frontArticle", frontPost)
+                .render("olderArticles", olderPosts);
+
     }
 }
