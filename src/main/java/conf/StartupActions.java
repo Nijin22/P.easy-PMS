a48966fb2a9316b497d33e4ef6024636457760d6
Dennis Weber <nijin22@gmail.com>
2017-04-22 11:56:50 +0200

    HUGE cleanup from old ninja-files

--- src/main/java/conf/StartupActions.java
+++ src/main/java/conf/StartupActions.java
@@ -7,30 +7,21 @@ import ninja.utils.NinjaProperties;
 
 import com.google.inject.Inject;
 
-import dao.SetupDao;
-
 @Singleton
 public class StartupActions {
-    
-    @Inject
-    SetupDao setupDao;
 
-    private NinjaProperties ninjaProperties;
+	@SuppressWarnings("unused") // Might be useful later...
+	private NinjaProperties ninjaProperties;
 
-    @Inject
-    public StartupActions(NinjaProperties ninjaProperties) {
-        this.ninjaProperties = ninjaProperties;
-    }
-    
-    @Start(order=100)
-    public void generateDummyDataWhenInTest() {
-        
-        if (!ninjaProperties.isProd()) {
-            
-            setupDao.setup();
-            
-        }
-        
-    }
+	@Inject
+	public StartupActions(NinjaProperties ninjaProperties) {
+		this.ninjaProperties = ninjaProperties;
+	}
+
+	@Start(order = 100)
+	public void generateDummyDataWhenInTest() {
+		// We can generate example data in test mode here.
+
+	}
 
 }
