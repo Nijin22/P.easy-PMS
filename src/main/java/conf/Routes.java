3c9dc7dd52d9c34f0536fcfc2b979e3c03ecc414
Haleo <julian.voegt@googlemail.com>
2017-05-27 17:24:25 +0200

    Several HTML changes by @Haleo (#17)
    * Added Internationalization
    
    Added internat. for:
    
    Account
    Register
    Organisation
    
    * Internationalization added
    
    Added internationalization for:
    Impress
    MyCalendar
    Members
    Task
    Navigation
    Register
    FileUpload
    Projekt
    Footer
    
    * Corrected Footer Layout
    
    * Added missing closing sign
    
    * Fixed Internationalization
    
    * Register redone, CSS-Files fixed
    
    * Added CreateMilestone, corrected Layout.ftl, corrected Footer position
    
    * Project edited
    
    New description field
    editable project names

--- src/main/java/conf/Routes.java
+++ src/main/java/conf/Routes.java
@@ -53,7 +53,6 @@ public class Routes implements ApplicationRoutes {
         router.GET().route("/forgotPassword").with(ApplicationController::forgotPassword);
         router.GET().route("/myCalender").with(ApplicationController::myCalender);
         router.GET().route("/organization/{id}").with(ApplicationController::organization);
-       
         router.GET().route("/projects").with(ApplicationController::projects);
         router.GET().route("/projects/{projectID}").with(ApplicationController::project);
         router.GET().route("/projects/{projectID}/members").with(ApplicationController::members);
