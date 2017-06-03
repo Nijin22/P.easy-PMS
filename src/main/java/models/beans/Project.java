6089af708488f80fcf40f32eee5a77b2a0e041b8
Dennis Weber <nijin22@gmail.com>
2017-06-03 12:01:23 +0200

    Removes unused import
    which prevented build on computers without the apache regex engine

--- src/main/java/models/beans/Project.java
+++ src/main/java/models/beans/Project.java
@@ -8,14 +8,11 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
-
-import org.apache.regexp.recompile;
 
 import models.beans.PeasyUser;
 import javax.persistence.ManyToMany;
